3fe568143abc75834bd1cb4157aaecfb899d55b0
azhuchkov <azhuchkov@zuzex.com>
2025-07-25 10:18:21 +0300

    Работа с регистрациец PW

---
+++ tests/test_registration.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_successfull_registration():
+    with sync_playwright() as playwright:
+
+        browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user.name@gmail.com')
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        context.storage_state(path='browser_state.json')
+
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser_state.json')
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
