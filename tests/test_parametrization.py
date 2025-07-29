1d45c692ccc9af2d22ea99ee9dde6feb7ccc7a68
azhuchkov <azhuchkov@zuzex.com>
2025-07-29 14:35:06 +0300

    Параметризация Pytest

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -1,4 +1,5 @@
 import pytest
+from _pytest.fixtures import SubRequest
 
 @pytest.mark.parametrize("number", [1, 2, 3, -1])
 def test_numbers(number: int):
@@ -12,3 +13,41 @@ def test_several_numbers(number: int, expected: int):
 @pytest.mark.parametrize("browser", ["chromium", "webkit", "firefox"])
 def test_multiplication_of_numbers(os: str, browser: str):
     assert len(os + browser) > 0
+
+@pytest.fixture(params=['chromium', 'webkit', 'firefox'])
+def browser(request: SubRequest):
+    return request.param
+
+
+def test_open_browser(browser: str):
+    print(f'Running test on browser: {browser}')
+
+@pytest.mark.parametrize('user', ['Alice', 'Zara'])
+class TestOperations:
+    
+    @pytest.mark.parametrize('account', ['Credit card', 'Debit card'])
+    def test_user_with_operations(self, user: str, account: str):
+        print(f'User with operation: {user}')
+
+    def test_user_without_operations(self, user: str):
+        print(f'User without operation: {user}')
+
+
+users = {
+    '+79992223344': 'User with money on bank account', 
+    '+79992223343': 'User without money on bank account', 
+    '+79992223342': 'User with operations on bank account'
+}
+    
+
+def format_phone_number(phone_number: str) -> str:
+        return f'{phone_number}: {users[phone_number]}'
+
+@pytest.mark.parametrize(
+        'phone_number', 
+        users.keys(),
+        ids=format_phone_number
+        #ids=lambda phone_number: f'{phone_number}: {users[phone_number]}'
+)
+def test_identifiers(phone_number: str):
+    ...
