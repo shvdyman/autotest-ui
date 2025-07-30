3b3a1cf441c885fa3845b3875e535eac92dc188f
azhuchkov <azhuchkov@zuzex.com>
2025-07-30 14:12:25 +0300

    PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,8 +1,9 @@
 import pytest
 from playwright.sync_api import expect, Page
-from pages.login_page import LoginPage
+from pages.login_page import LoginPage # Импортируем LoginPage
 
 @pytest.mark.regression
+@pytest.mark.authorization
 @pytest.mark.parametrize(
     "email, password",
     [
@@ -12,8 +13,7 @@ from pages.login_page import LoginPage
     ]
 )
 
-def test_wrong_email_or_password_authorization(chromium_page: Page, email:str, password: str):
-    login_page = LoginPage(page=chromium_page)
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email:str, password: str):
     login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
     login_page.fill_login_form(email=email, password=password)
     login_page.click_login_button()
