7cbec7ba165ae089466542dd1ee8415139ec7413
azhuchkov <azhuchkov@zuzex.com>
2025-08-01 12:21:25 +0300

    Практикуемся в реализации PageObject

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 pytest_plugins = (
     "fixtures.pages" # Подключаем фикстуры страниц
     "fixtures.browsers"
+    
 )
--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
@@ -12,19 +13,20 @@ def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
 
 
-# Эта фикстура должна быть добавлена после выполнения домашнего задания предыдущего урока
 @pytest.fixture
 def registration_page(chromium_page: Page) -> RegistrationPage:
     return RegistrationPage(page=chromium_page)
 
 
-# Эта фикстура должна быть добавлена после выполнения домашнего задания предыдущего урока
 @pytest.fixture
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
 
-# Добавили эту фикстуру в данном шаге
 @pytest.fixture
 def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
