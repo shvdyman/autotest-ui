1743e305d5dd621dc02f842eff4ae13c0a3938eb
azhuchkov <azhuchkov@zuzex.com>
2025-08-11 12:04:30 +0300

    Знакомство с PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 from components.navigation.navbar_component import NavbarComponent
+from components.navigation.sidebar_component import SidebarComponent
 from pages.base_page import BasePage
 
 
