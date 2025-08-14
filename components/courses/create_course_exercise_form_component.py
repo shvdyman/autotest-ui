f9c2bfd95ceb47fa9a448e4ab31fb3a9c861e7a5
azhuchkov <azhuchkov@zuzex.com>
2025-08-14 13:42:27 +0300

    PageFactory

--- components/courses/create_course_exercise_form_component.py
+++ components/courses/create_course_exercise_form_component.py
@@ -1,17 +1,35 @@
 from playwright.sync_api import Page, expect
 from components.base_component import BaseComponent
+from elements.button import Button
+from elements.text import Text
+from elements.input import Input
 
 class CreateCourseExerciseFormComponent(BaseComponent):
+    def __init__(self, page: Page):
+        super().__init__(page)
+    
+        self.delete_exercise_button = Button(page, 'create-course-exercise-{index}-box-toolbar-delete-exercise-button', 'Delete exercise')
+        self.subtitle = Text(page, 'create-course-exercise-{index}-box-toolbar-subtitle-text', 'Exercise subtitle')
+        self.title_input = Input(page, 'create-course-exercise-form-title-{index}-input', 'Title')
+        self.description_input = Input(page, 'create-course-exercise-form-description-{index}-input', 'Description')
+    
+    
     def click_delete_button(self, index: int):
-        delete_button = self.page.get_by_test_id(
-            f"create-course-exercise-{index}-box-toolbar-delete-exercise-button"
-        )
-        delete_button.click()
+        self.delete_exercise_button.click(index=index)
 
     def check_visible(self, index: int, title: str, description: str):
         subtitle = self.page.get_by_test_id(f"create-course-exercise-{index}-box-toolbar-subtitle-text")
         title_input = self.page.get_by_test_id(f"create-course-exercise-form-title-{index}-input")
         description_input = self.page.get_by_test_id(f"create-course-exercise-form-description-{index}-input")
+
+        self.subtitle.check_visible()
+        self.subtitle.check_have_text(f"#{index + 1} Exercise", index=index)
+        
+        self.title_input.check_visible()
+        self.title_input.check_have_value(title, index=index)
+
+        self.description_input.check_visible()
+        self.description_input.check_have_value()
 
         expect(subtitle).to_be_visible()
         expect(subtitle).to_have_text(f"#{index + 1} Exercise")
