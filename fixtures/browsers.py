a1cbb50e77a26769c5adf3f68782550448f47239
shvdy_man <shvdyman@gmail.com>
2025-08-21 15:29:02 +0300

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,19 +4,13 @@ from playwright.sync_api import Page, Playwright
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
 import allure
+from allure_commons.types import AttachmentType
+from tools.playwright.pages import initialize_playwright_page
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    yield context.new_page()
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
     
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -33,13 +27,5 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page: 
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state="browser-state.json")
