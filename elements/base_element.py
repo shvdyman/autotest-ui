4a70d218abc9461d5c6bfa2ab05756c669d86385
shvdy_man <shvdyman@gmail.com>
2025-08-21 10:03:38 +0300

    Добавление Allure шагов в автотесты

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page, Locator, expect
-
+import allure
 
 class BaseElement:
     def __init__(self, page: Page, locator: str, name: str):
@@ -7,21 +7,29 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
+    @property
+    def type_of(self) -> str:
+        return "base element"
+
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)
-        return self.page.get_by_test_id(locator).nth(nth)
+        with allure.step(f'Getting locator with "data-testid={locator}" at index "{nth}"'):
+            return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self, nth: int = 0, **kwargs):
-        # Добавили аргумент nth и передеаем его в get_locator
-        locator = self.get_locator(nth, **kwargs)
-        locator.click()
+        with allure.step(f'Clicking {self.type_of} "{self.name}"'):
+            # Добавили аргумент nth и передеаем его в get_locator
+            locator = self.get_locator(nth, **kwargs)  
+            locator.click()
 
     def check_visible(self, nth: int = 0, **kwargs):
-        # Добавили аргумент nth и передеаем его в get_locator
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_be_visible()
+        with allure.step(f'Checking that {self.type_of} "{self.name}" is visible'):
+            # Добавили аргумент nth и передеаем его в get_locator
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_be_visible()
 
     def check_have_text(self, text: str, nth: int = 0, **kwargs):
-        # Добавили аргумент nth и передеаем его в get_locator
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_have_text(text)
+        with allure.step(f'Checking that {self.type_of} "{self.name}" has text "{text}"'):
+            # Добавили аргумент nth и передеаем его в get_locator
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_have_text(text)
--- elements/button.py
+++ elements/button.py
@@ -1,15 +1,20 @@
 from playwright.sync_api import expect
-
+import allure
 from elements.base_element import BaseElement
 
 
 class Button(BaseElement):
+    def type_of(self) -> str:
+        return "button"
+    
+    @property
     def check_enabled(self, nth: int = 0, **kwargs):
-        # Добавили аргумент nth и передеаем его в get_locator
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_be_enabled()
-
+        with allure.step(f'Checking that {self.type_of} "{self.name}" is enabled'):
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_be_enabled()
+    
     def check_disabled(self, nth: int = 0, **kwargs):
-        # Добавили аргумент nth и передеаем его в get_locator
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_be_disabled()
+        with allure.step(f'Checking that {self.type_of} "{self.name}" is disabled'):
+            # Добавили аргумент nth и передеаем его в get_locator
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_be_disabled()
