a880fb01f50afd08f3370e7bef462cf86be14545
shvdy_man <shvdyman@gmail.com>
2025-08-22 14:13:28 +0300

    Запуск автотестов на разных браузерах

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,15 +1,17 @@
 import allure
 from playwright.sync_api import Playwright, Page
 
-from config import settings
+from config import settings, Browser  # Импортируем enum Browser
 
 
 def initialize_playwright_page(
         playwright: Playwright,
         test_name: str,
+        browser_type: Browser,  # Передаем бразуер в качестве аргумента
         storage_state: str | None = None
 ) -> Page:
-    browser = playwright.chromium.launch(headless=settings.headless)
+    # Динамически получаем нужный браузер
+    browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(
         base_url=settings.get_base_url(),
         storage_state=storage_state,
